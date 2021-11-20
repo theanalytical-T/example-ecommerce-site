6cddb98211cce6a4cd1d60668cafd3b5d9739f43
Tiara Brunt <53661450+tiara143@users.noreply.github.com>
2021-11-20 17:06:06 -0600

    11/20/2021
    Added another div to group the product item images..

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,5 @@
+
+
 const menuBtn = document.querySelector('.menu-btn')
 
 let menuOpen = false;
@@ -6,25 +8,73 @@ menuBtn.addEventListener("click", () => {
     if (!menuOpen) {
         menuBtn.classList.add('open');
         menuOpen = true;
+        document.querySelector(".overlay").style.display = "block";
+        document.querySelector(".nav-links").style.display = "block";
+        // document.querySelector(".menu").style.display = "block";
+
     } else {
         menuBtn.classList.remove('open');
         menuOpen = false;
+        document.querySelector(".overlay").style.display = "none";
+        document.querySelector(".nav-links").style.display = "none";
+        // document.querySelector(".menu").style.display = "none";
     }
 });
 
-for (let hashtag = "#"; hashtag.length < 8; hashtag += "#") {
-    console.log(hashtag);
-    break;
-}
 
-for (let counter = 0; counter.length < 101; counter++) {
-    if (counter % 3 === 0) {
-        console.log("Fizz");
-    } else if (counter % 5 === 0) {
-        console.log("Buzz");
-    } else if (counter % 3 === 0 && counter % 5 === 0) {
-        console.log("FizzBuzz");
-    } else {
-        console.log(counter);
-    }
-}
+// const slides = document.getElementsByClassName('carousel-item');
+// let slidePosition = 0;
+// const totalSlides = slides.length;
+
+// document.getElementById('carousel-button-next').addEventListener('click', moveToNextSlide);
+// document.getElementById('carousel-button-prev').addEventListener('click', moveToPrevSlide);
+
+// function hideAllSlides() {
+//     for (let slide of slides) {
+//         slide.classList.remove('carousel-item-visible');
+//         slide.classList.add('carousel-item-hidden');
+//     }
+// }
+
+// function moveToNextSlide() {
+//     hideAllSlides();
+
+//     if (slidePosition === totalSlides - 1) {
+//         slidePosition = 0;
+//     } else {
+//         slidePosition++;
+//     }
+
+//     slides[slidePosition].classList.add("carousel-item-visible");
+// }
+
+// function moveToPrevSlide() {
+//     hideAllSlides();
+
+//     if (slidePosition === 0) {
+//         slidePosition = totalSlides - 1;
+//     } else {
+//         slidePosition--;
+//     }
+
+//     slides[slidePosition].classList.add("carousel-item-visible");
+// }
+
+
+
+// for (let hashtag = "#"; hashtag.length < 8; hashtag += "#") {
+//     console.log(hashtag);
+//     break;
+// }
+
+// for (let counter = 0; counter.length < 101; counter++) {
+//     if (counter % 3 === 0) {
+//         console.log("Fizz");
+//     } else if (counter % 5 === 0) {
+//         console.log("Buzz");
+//     } else if (counter % 3 === 0 && counter % 5 === 0) {
+//         console.log("FizzBuzz");
+//     } else {
+//         console.log(counter);
+//     }
+// }
